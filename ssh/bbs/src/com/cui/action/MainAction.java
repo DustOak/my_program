4466974907e7246bbd2de7642d13cfc587b78ce8
ChangFengcwb <cuiwenbin@cwb95.xyz>
2019-05-08 09:27:21 +0800

    update

--- ssh/bbs/src/com/cui/action/MainAction.java
+++ ssh/bbs/src/com/cui/action/MainAction.java
@@ -1,14 +1,32 @@
 package com.cui.action;
 
 import com.cui.po.Board;
-import com.cui.service.BoardLoad;
+import com.cui.service.BoardLoadService;
+import com.cui.util.ApplicationContextOperator;
 import com.opensymphony.xwork2.ActionSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 
 import java.util.List;
 
+@Component
 public class MainAction extends ActionSupport {
 List<Board> boards;
-BoardLoad boardLoad;
+
+@Autowired
+BoardLoadService boardLoad;
+
+public String execute() {
+	for (String s : ApplicationContextOperator.getApplicationContext().getBeanDefinitionNames()) {
+		System.out.println(s);
+	}
+	boards = boardLoad.loadAllBoards();
+//	for (int i = 0; i < boards.size(); i++) {
+//		System.out.println(boards.get(i).getAid());
+//	}
+	return INPUT;
+}
 
 public List<Board> getBoards() {
 	return boards;
@@ -18,11 +36,11 @@ public void setBoards(List<Board> boards) {
 	this.boards = boards;
 }
 
-public BoardLoad getBoardLoad() {
+public BoardLoadService getBoardLoad() {
 	return boardLoad;
 }
 
-public void setBoardLoad(BoardLoad boardLoad) {
+public void setBoardLoad(BoardLoadService boardLoad) {
 	this.boardLoad = boardLoad;
 }
 
